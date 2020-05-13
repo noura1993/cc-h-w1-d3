4d894e2e82c2612c66bbaab91f9eab6408772337
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-05-13 20:12:35 +0100

    Print the positon of Linlithgow

--- arrays_hashes_quiz_start_point/exercise_a_stops.rb
+++ arrays_hashes_quiz_start_point/exercise_a_stops.rb
@@ -10,6 +10,9 @@ stops.unshift("Glasgow Queen St")
 stops.insert(4, "Polmont")
 
 #4. Print out the index position of "Linlithgow"
+index_of_linlithgow = stops.index("Linlithgow")
+p "The index position of Linlithgow is #{index_of_linlithgow}"   
+
 #5. Remove "Livingston" from the array using its name
 #6. Delete "Cumbernauld" from the array by index
 #7. Print the number of stops there are in the array?
