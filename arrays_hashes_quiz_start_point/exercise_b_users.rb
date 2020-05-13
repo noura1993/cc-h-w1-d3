afb19873abd22e57a2168fd2d401ba1242d2ee15
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-05-13 23:09:26 +0100

    Get Jonathan's Twitter

--- arrays_hashes_quiz_start_point/exercise_b_users.rb
+++ arrays_hashes_quiz_start_point/exercise_b_users.rb
@@ -1,4 +1,4 @@
-users = {
+ users = {
   "Jonathan" => {
     :twitter => "jonnyt",
     :lottery_numbers => [6, 12, 49, 33, 45, 20],
@@ -55,6 +55,8 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+p users["Jonathan"][:twitter]
+
 # 2. Get Erik's hometown
 # 3. Get the array of Erik's lottery numbers
 # 4. Get the species of Avril's pet Monty
