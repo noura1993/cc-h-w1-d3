78785738a595fda8a0bcfd51a7c333316d56d678
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-05-13 20:33:34 +0100

    Remove Cumbernauld by index

--- arrays_hashes_quiz_start_point/exercise_a_stops.rb
+++ arrays_hashes_quiz_start_point/exercise_a_stops.rb
@@ -17,6 +17,8 @@ p "The index position of Linlithgow is #{index_of_linlithgow}"
 stops.delete("Livingston")
 
 #6. Delete "Cumbernauld" from the array by index
+stops.delete_at(2)
+
 #7. Print the number of stops there are in the array?
 #8. Show as many ways as you can to return "Falkirk High" from the array?
 #9. Reverse the positions of the stops in the array
